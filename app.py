26e246a5a9af329be2e63ec778c1422de680bac1
Yuri Souza Santos <yurisouza1712@gmail.com>
2022-08-12 20:24:08 -0300

    feat: adding reparse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import reqparse, Resource, Api
 from flask_mongoengine import MongoEngine
 
 
@@ -15,6 +15,34 @@ app.config["MONGODB_SETTINGS"] = [
       "password": "admin",
     }
 ]
+
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('first_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('last_name',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('cpf',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('birth_date',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
 
 
 api = Api(app)
@@ -37,7 +65,8 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"message": "teste"}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
 
     def get(self, cpf):
         return {"message": "CPF"}
