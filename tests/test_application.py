47f203b5ce6149831ca03327f904a3aee78c072a
Yuri Souza Santos <yurisouza1712@gmail.com>
2022-09-26 19:12:28 -0300

    test: add test unity to post valid and invalid users

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,35 @@ class TestApplication():
         app = create_app('config.MockConfig')
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "Yuri",
+            "last_name": "Maximo",
+            "cpf": "957.694.960-29",
+            "email": "conta@yuri.me",
+            "birth_date": "1993-12-17"
+        }
+    
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "Yuri",
+            "last_name": "Maximo",
+            "cpf": "957.694.960-27",
+            "email": "conta@yuri.me",
+            "birth_date": "1993-12-17"
+        }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
